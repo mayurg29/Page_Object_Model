1622e211fd3343c86f148eef7d665dcd834b19a9
mayurg29 <97237587+mayurg29@users.noreply.github.com>
2022-09-24 19:09:57 +0530

    Updated base class

--- Page_Object_Model/src/main/java/base/Base.java
+++ Page_Object_Model/src/main/java/base/Base.java
@@ -2,7 +2,6 @@ package base;
 
 import java.io.FileInputStream;
 import java.util.Properties;
-
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
