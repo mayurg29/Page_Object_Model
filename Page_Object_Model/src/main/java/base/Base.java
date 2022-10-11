551ddb5c14d62388422ac5b2c0bd7af6173c26b5
mayurg29 <97237587+mayurg29@users.noreply.github.com>
2022-10-12 00:10:30 +0530

    Updated Base class

--- Page_Object_Model/src/main/java/base/Base.java
+++ Page_Object_Model/src/main/java/base/Base.java
@@ -1,15 +1,25 @@
 package base;
 
+import java.awt.Robot;
 import java.io.FileInputStream;
 import java.util.Properties;
+
+import org.apache.log4j.Logger;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
+import pages.FindFriendsPage;
+import pages.HomePage;
 import pages.LoginPage;
+import pages.MarketplacePage;
+import pages.ProfilePage;
+import utility.LoggerExample;
 import utility.Utility;
 
 public class Base {
@@ -25,10 +35,18 @@ public class Base {
 	protected static XSSFCell xCell;
 	protected static XSSFRow xRow;
 	protected static Object[][] data;
+	protected static Robot robot;
+	protected static String message;
+	protected static Actions actions; 
 	
 	protected static LoginPage loginPage;
-	protected static String message;
+	protected static HomePage homePage;
+	protected static ProfilePage profilePage;
+	protected static MarketplacePage marketplacePage;
+	protected static FindFriendsPage findFriendsPage;
 	
+	protected static Logger logger = Logger.getLogger(LoggerExample.class.getName());
+
 	protected final static String FILE_PATH = System.getProperty("user.dir") + "\\src\\main\\resources\\repository\\LoginTestData.xlsx";
 	
 	static {
