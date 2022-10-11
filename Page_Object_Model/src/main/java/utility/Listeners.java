59949b9759799dff80a581b8d4771f50e2eb3841
mayurg29 <97237587+mayurg29@users.noreply.github.com>
2022-10-12 00:14:01 +0530

    Updated listeners class

--- Page_Object_Model/src/main/java/utility/Listeners.java
+++ Page_Object_Model/src/main/java/utility/Listeners.java
@@ -4,8 +4,11 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class Listeners implements ITestListener{
+import base.Base;
 
+public class Listeners extends Base implements ITestListener{
+
+	Base base = new Base();
 	Utility utility = new Utility();
 
 	public void onStart(ITestContext context) {	
