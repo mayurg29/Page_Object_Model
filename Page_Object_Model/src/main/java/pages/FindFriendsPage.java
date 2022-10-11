f308bb68e6bbe58231fccca024d7bcb8fde21851
mayurg29 <97237587+mayurg29@users.noreply.github.com>
2022-10-12 00:17:18 +0530

    Added Find Friends page

---
+++ Page_Object_Model/src/main/java/pages/FindFriendsPage.java
@@ -0,0 +1,38 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import base.Base;
+
+public class FindFriendsPage extends Base{
+
+	@FindBy(xpath = "//a[@aria-label='Find friends']")
+	private WebElement findFriends;
+
+	@FindBy(xpath = "//span[contains(text(),'All Friends')]")
+	private WebElement allFriends;
+
+	@FindBy(xpath = "//input[@placeholder='Search Friends']")
+	private WebElement searchFriends;
+
+	public FindFriendsPage() {
+		PageFactory.initElements(driver, this);
+	}
+
+	/**
+	 * Traversing to All friends
+	 */
+	@SuppressWarnings("static-access")
+	public void findFriends() {
+		try {
+			utility.click(driver, 10, findFriends);
+			utility.click(driver, 10, allFriends);
+			Thread.sleep(3000);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
